d3c05a3b267d1b428cd997ca867cd0f6ead37078
Virus Kei <56038340+virusky@users.noreply.github.com>
2021-03-28 22:30:15 +0700

    28-3-2021 Lab 2

---
+++ app/src/main/java/iuh/edu/lab2/Activity3rd.java
@@ -0,0 +1,14 @@
+package iuh.edu.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity3rd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity3rd);
+    }
+}
